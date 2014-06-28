f9da17fe36e69584c5d9f5faf99b00bdf708b2ff
wickY26 <poyrazthewicked@gmail.com>
2014-06-28 06:49:51 -0700

    ...

--- app/routes/poems.server.routes.js
+++ app/routes/poems.server.routes.js
@@ -3,17 +3,18 @@
 module.exports = function (app) {
 	var users = require('../../app/controllers/users');
 	var poems = require('../../app/controllers/poems');
+	var topics = require('../../app/controllers/topics');
 
 	// Poems Routes
-	app.route('/poems')
+	app.route('/poems/:topicId')
 		.get(poems.list)
 		.post(users.requiresLogin, poems.create);
 
-	app.route('/poems/:poemId')
-		.get(poems.read)
-		.put(users.requiresLogin, poems.hasAuthorization, poems.update)
-		.delete(users.requiresLogin, poems.hasAuthorization, poems.delete);
+	app.route('/poems/:topicId/:poemId')
+		.put(users.requiresLogin, poems.hasPermission, poems.hasAuthorization, poems.update)
+		.delete(users.requiresLogin, poems.hasPermission, poems.hasAuthorization, poems.delete);
 
 	// Finish by binding the Poem middleware
 	app.param('poemId', poems.poemByID);
+	app.param('topicId', topics.topicByID);
 };

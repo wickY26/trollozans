a8be1faa0d3130c0959892cd5c83e02dc9822fda
Poorh4n <burhankarakas@gmail.com>
2014-06-28 13:47:15 +0300

    grunt env option
    Grunt --env gecerek Node_env set edildi

--- gruntfile.js
+++ gruntfile.js
@@ -143,9 +143,11 @@ module.exports = function(grunt) {
 
 	// Making grunt default to force in order not to break the project.
 	grunt.option('force', true);
+	process.env.NODE_ENV = grunt.option('env');
 
 	// A Task for loading the configuration object
 	grunt.task.registerTask('loadConfig', 'Task that loads the config into a grunt option.', function() {
+		
 		var init = require('./config/init')();
 		var config = require('./config/config');
 

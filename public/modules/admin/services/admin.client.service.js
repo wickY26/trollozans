071bd08bec92c5e70b3b55ee9730e1e1b6316956
Poorh4n <burhankarakas@gmail.com>
2014-07-08 01:03:47 +0300

    New Angular Module Admin
    Admin module structure with all capabilities

--- public/modules/admin/services/admin.client.service.js
+++ public/modules/admin/services/admin.client.service.js
@@ -1,6 +1,10 @@
 'use strict';
 
-//Topics service used to communicate Topics REST endpoints
+/**
+ * Restangular Service For Admin operations
+ * @param  {[type]} Restangular) {		return    Restangular.service('admin');	}] [description]
+ * @return {[type]}              [description]
+ */
 angular.module('admin').factory('Admin', ['Restangular',
 	function (Restangular) {
 		return Restangular.service('admin');
